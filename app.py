a4425c37886dcbb18be13e49e047dd3982ae885b
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-11-15 13:45:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -80,12 +80,13 @@ if user_question:
           print(cb)
         st.write(response)
 
-        tld='es'
-        tts = gTTS(response,"es", tld, slow=False)
-        result, output_text = text_to_speech(response, tld)
-        audio_file = open(f"temp/{result}.mp3", "rb")
-        audio_bytes = audio_file.read()
-        st.markdown(f"## Escucha:")
-        st.audio(audio_bytes, format="audio/mp3", start_time=0)
+    if st.button("convertir"):
+       result, output_text = text_to_speech(text, tld)
+       audio_file = open(f"temp/{result}.mp3", "rb")
+       audio_bytes = audio_file.read()
+       st.markdown(f"## Tú audio:")
+       st.audio(audio_bytes, format="audio/mp3", start_time=0)
+
+
 
 
